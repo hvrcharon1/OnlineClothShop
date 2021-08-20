1ecbdfa8f5e68960385f929a1f911e4a9534837e
cloud-parrot-1 <harshalrasal1@gmail.com>
2021-08-20 16:58:02 -0400

    .env error resolved

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 //import { classExpression } from 'babel-types';
+import { Link } from 'react-router-dom';
 
 import logo from "../../assets/cloths.png"
 import useStyles from './styles';
@@ -13,12 +14,13 @@ const Navbar = ({ totalItems }) => {
             
             <AppBar position='fixed' className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to = "/" variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Commerce.js" height="25px" className={classes.image} /> Commerce.js
                     </Typography>
                     <div className={classes.grow} />
                     <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
+                        <Link to = "/cart">go to cart</Link>
+                        <IconButton component={Link} to = "/cart" aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
